e9f7547fd1d8c429ec214541d8c78ec155125a98
fellipeutaka <fellipeutaka@gmail.com>
2022-09-10 21:38:15 -0300

    * feat: board tests;

---
+++ src/utils/board.spec.ts
@@ -0,0 +1,32 @@
+import { expect, test } from "vitest";
+import { getCurrentPlayer, getIsGameOver, getWinner } from "./board";
+
+const emptyBoard = Array<string>(9).fill("");
+
+test("get current player", () => {
+  const player = getCurrentPlayer(emptyBoard);
+  expect(player).toEqual("X");
+  {
+    const board = ["X", "", "", "O", "", "X", "", "", ""];
+    const player = getCurrentPlayer(board);
+    expect(player).toEqual("O");
+  }
+});
+
+test("get winner", () => {
+  const victoriousBoard = [
+    { winner: "X", board: ["X", "X", "X", "O", "", "", "O", "", ""] },
+    { winner: "O", board: ["", "X", "X", "O", "O", "O", "", "", ""] },
+    { winner: null, board: ["", "", "", "", "", "", "", "", ""] },
+  ];
+  victoriousBoard.forEach((victorious) => {
+    const winner = getWinner(victorious.board);
+    expect(winner).toEqual(victorious.winner);
+  });
+});
+
+test("get game is over", () => {
+  const overedBoard = ["X", "X", "X", "O", "", "", "O", "", ""];
+  expect(getIsGameOver(overedBoard)).toBe(true);
+  expect(getIsGameOver(emptyBoard)).toBe(false);
+});
